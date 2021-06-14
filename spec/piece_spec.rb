e6587bd014c43754dede52a11a5be6cbabce8a55
matthew-deluge <matthewj913@gmail.com>
2021-06-14 18:57:36 -0400

    fixed Rook tests

--- spec/piece_spec.rb
+++ spec/piece_spec.rb
@@ -22,13 +22,13 @@ describe Rook do
   describe '#valid_move?' do
     subject(:rook) {described_class.new('white','R')}
     it 'returns true for a vertical move' do
-      expect(rook.valid_move?([0, 0], [0, 1])).to be(true)
+      expect(rook.valid_move?([1, 1], [1, 2])).to be(true)
     end
     it 'returns true for a horizontal move' do
-      expect(rook.valid_move?([0, 0], [3, 0])).to be(true)
+      expect(rook.valid_move?([1, 1], [4, 1])).to be(true)
     end
     it 'returns false for a non-horizontal or vertical move' do
-      expect(rook.valid_move?([0, 0], [3, 4])).to be(false)
+      expect(rook.valid_move?([1, 1], [3, 4])).to be(false)
     end
   end
 
