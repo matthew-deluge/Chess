47fa560a4060b9f9a6e96f9b9d58c827102ce525
matthew-deluge <matthewj913@gmail.com>
2021-06-14 18:54:35 -0400

    added #valid_move? for king

--- lib/pieces/king.rb
+++ lib/pieces/king.rb
@@ -2,34 +2,55 @@
 require_relative './piece'
 
 # class for the king piece
-Class King < Piece
+class King < Piece
 
   def valid_move?(initial, final)
     return false if final[0] <= 0 || final[1] <= 0
 
-    if initial[0] - 1 == final[0] && initial[1] == final[1] # down one
-      true
-    elsif initial[0] + 1 == final[0] && initial[1] == final[1] # up one
-      true
-    elsif initial[1] - 1 == final[1] && initial[0] == final[0] # left one
-      true
-    elsif initial[1] + 1 == final[1] && inital[0] == final[0] #right one
-      true
-    elsif inital[0] + 1 == final[0] && (initial[1]+1 == final[1] || initial[1] - 1 == final[1]) # Diagnols up
-      true
-    elsif initial[0] - 1 == final[0] && (initial[1]+1 == final[1] || initial[1] - 1 == final[1]) # Diagnols down
-      true
-    else
-      false
-    end
+    valid_vertical?(initial,final)||valid_horizontal?(initial,final)||valid_diaganol?(initial, final)
   end
 
   def generate_path(initial, final)
     [intial, final]
   end
 
+
   private
 
+  def valid_horizontal?(initial, final)
+    valid_horizontal_down?(initial, final) || valid_horizontal_up?(initial, final)
   end
+
+  def valid_horizontal_up?(initial, final)
+    initial[1] + 1 == final[1] && initial[0] == final[0]
+  end
+
+  def valid_horizontal_down?(initial, final)
+    initial[1] - 1 == final[1] && initial[0] == final[0] # left one
+  end
+
+  def valid_vertical?(initial, final)
+    valid_vertical_up?(initial, final) || valid_vertical_down?(initial, final)
+  end
+
+  def valid_vertical_down?(initial, final)
+    initial[0] - 1 == final[0] && initial[1] == final[1] # down one
+  end
+
+  def valid_vertical_up?(initial, final)
+    initial[0] + 1 == final[0] && initial[1] == final[1]
+  end
+
+  def valid_diaganol?(initial, final)
+    valid_diagnol_up?(initial, final) || valid_diagnol_down?(initial, final)
+  end
+
+  def valid_diagnol_up?(initial, final)
+    initial[0] + 1 == final[0] && (initial[1]+1 == final[1] || initial[1] - 1 == final[1])
+  end
+
+  def valid_diagnol_down?(initial, final)
+    initial[0] - 1 == final[0] && (initial[1]+1 == final[1] || initial[1] - 1 == final[1])
+  end
+
 end
-  
