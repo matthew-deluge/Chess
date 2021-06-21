45f26a4b1e81314b4b9fc9d4098cd1d89f8e19c1
matthew-deluge <matthewj913@gmail.com>
2021-06-21 17:58:52 -0400

    Wrote first draft of #checkmate?

--- lib/pieces/king.rb
+++ lib/pieces/king.rb
@@ -25,7 +25,49 @@ class King < Piece
     false
   end
 
+  def checkmate?(king_square, board)
+    return false unless king_square.piece.check?
+
+    no_available_blockers?(king_square, board) || no_escape_path?(king_square, board)
+
+
+    
+
   private
+
+  def no_escape_path?(king_square, board)
+    board.node_array.each do |square|
+      if valid_move?(king_square, square.coord)
+        board_copy = board
+        board_copy.move(king_square, square.coord)
+        return false unless check?(king_square, board_copy)
+      end
+    end
+    true
+  end
+  
+  def no_available_blockers?(king_square, board)
+    board.node_array.each do |square|
+      next if square.piece.nil?||square.piece.color != @color
+
+      original_coord = square.coord
+      board.node_array.each do |potential_square|
+        potential_coord = potential_square.coord
+        board_copy = board
+        if board.clear_path?(original_coord, potential_coord)
+          board.move(original_coord, potential_coord)
+          if check?(king_square, board_copy)
+            next
+          else
+            false
+          end
+        end
+      end
+    end
+    true
+  end
+
+        potential_square.coord
 
   def valid_horizontal?(initial, final)
     valid_horizontal_down?(initial, final) || valid_horizontal_up?(initial, final)
