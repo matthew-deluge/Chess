4e634c8bf4c15e27076713e496992953edfa32e9
matthew-deluge <matthewj913@gmail.com>
2021-06-21 17:28:33 -0400

    updated arrangement with king piece

--- lib/arrangement.rb
+++ lib/arrangement.rb
@@ -1,18 +1,24 @@
 module Arrangement
 
   SET_UP_HASH = [
-      { piece: Rook.new('white','♜'),
-        coord: [1, 1] },
+    { piece: Rook.new('white', '♜'),
+      coord: [1, 1] },
 
-      { piece: Rook.new('white','♜'),
-        coord: [8, 1] },
+    { piece: King.new('white', '♚'),
+      coord: [4, 1] },
 
-      { piece: Rook.new('black','♖'),
-        coord: [1, 8]},
+    { piece: Rook.new('white', '♜'),
+      coord: [8, 1] },
 
-      { piece: Rook.new('black', '♖'),
-        coord: [8,8]}
-      ]
+    { piece: Rook.new('black', '♖'),
+      coord: [1, 8]},
+
+    { piece: King.new('black', '♔'),
+      coord: [4, 8]},
+
+    { piece: Rook.new('black', '♖'),
+      coord: [8,8]}
+    ]
 
   WHITE_PIECE_ARRAY = ['♚','♛','♝','♞','♟','♜']
   BLACK_PIECE_ARRAY = ['♗','♘','♙','♔','♕','♖']
