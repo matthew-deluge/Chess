feb2e04f3160c4b3ce2255b517ff365db9f6fa2c
matthew-deluge <matthewj913@gmail.com>
2021-07-02 11:51:11 -0400

    added coordinates to command line display of board

--- lib/display.rb
+++ lib/display.rb
@@ -4,23 +4,28 @@
 module Display
 
   def print_board(board)
+    counter = 8
     board.node_array.reverse.each do |square|
-      if square.coord[1] == 8
-        puts "\n"
-      end
       if square.piece.nil?
         if (square.coord[0]+square.coord[1]).even?
-          print "B".light_blue
+          print " B ".light_blue
         else
-          print "W"
+          print " W "
         end
       else 
-        print square.piece.symbol
+        print " #{square.piece.symbol} "
+      end
+      if square.coord[1] == 1
+        print "  #{counter}".green
+        puts "\n"
+        puts"\n"
+        counter -= 1
       end
     end
+    puts ' 8  7  6  5  4  3  2  1 '.green
   end
-end
-        
+
+end       
 
   # allows for colored strings: https://stackoverflow.com/questions/1489183/colorized-ruby-output-to-the-terminal
 class String
